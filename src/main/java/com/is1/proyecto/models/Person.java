9e8bc5c45e92dc7681c70ad7d19bdda63337e8aa
Joaquin Gonzalez <joaquingb2005@gmail.com>
2025-10-23 11:37:06 -0300

    dolor

--- src/main/java/com/is1/proyecto/models/Person.java
+++ src/main/java/com/is1/proyecto/models/Person.java
@@ -6,14 +6,7 @@ import org.javalite.activejdbc.annotations.Table;
 @Table("persona")
 public class Person extends Model {
 
-    // ActiveJDBC mapea automáticamente las columnas de la tabla 'users'
-    // (como 'id', 'name', 'password', etc.) a los atributos de esta clase.
-    // No necesitas declarar los campos (id, name, password) aquí como variables de instancia,
-    // ya que la clase Model base se encarga de la interacción con la base de datos.
-
-    // Opcional: Puedes agregar métodos getters y setters si prefieres un acceso más tipado,
-    // aunque los métodos genéricos de Model (getString(), set(), getInteger(), etc.) ya funcionan.
-
+    
     public Integer getDNI(){
         return getInteger("person_id");
     }
